0c4882dc7bea24b68a6b2e4ed61c290a500333ce
Claudio Pacheco <claudiopachecoramirez@gmail.com>
2023-04-25 23:32:37 -0400

    F-02

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,20 +1,43 @@
+import {useState,useEffect} from 'react';
+import axios from 'axios';
+import apiUrl from '../../api.js';
+
 export default function Carousel() {
+  let [categories,setCategories] = useState([]);
+  let [counter,setCounter] = useState(0);
+  useEffect(
+    ()=>{axios(apiUrl+'categories').then(res=>setCategories(res.data.categories)).catch(err=>(err))},
+    []
+  )
+  const next = () => {
+    setCounter(counter+1)
+    if (counter===categories.length-1){
+      setCounter(0)
+    }
+  }
+  const previous = () =>{
+    setCounter(counter-1)
+    if(counter===0){
+      setCounter(categories.length-1)
+    }
+  }  
+
     return (
       <>
         <div className="flex justify-center mb-8 mt-10 pt-10 xsm:hidden">
           <div className="bg-cover bg-gradient-to-r from-[#4338CA] to-[#5E52F3] flex xsm:hidden w-[90vw] h-[25vh] justify-around rounded-lg gap-1">
-            <div className="w-10 self-center">
+            <div onClick={previous} className="w-10 self-center">
               <svg xmlns="http://www.w3.org/2000/svg" fill="#FFFFFF80" viewBox="0 0 24 24" strokeWidth=".5" stroke="currentColor" className="w-12 h-10">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 9l-3 3m0 0l3 3m-3-3h7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
               </svg>
             </div>
-            <img className="self-center sm:h-[20vh] sm:w-30 sm:mb-8 lg:mb-14 md:h-48 md:mb-10 lg:h-80 " src="/images/deku_carousel.png" alt="characther_carousel" />
-            <img className="self-center sm:h-[25vh] sm:w-30 sm:mb-8 lg:mb-14 md:h-48 md:mb-10 lg:h-80 pb-5" src="/images/cover_carousel.png" alt="cover_carousel" />
+            <img className="self-center sm:h-[20vh] sm:w-30 sm:mb-8 lg:mb-14 md:h-48 md:mb-10 lg:h-80 " src={categories[counter]?.character_photo} alt="character_carousel" />
+            <img className="self-center sm:h-[25vh] sm:w-30 sm:mb-8 lg:mb-14 md:h-48 md:mb-10 lg:h-80 pb-5" src={categories[counter]?.cover_photo} alt="cover_carousel" />
             <div className="flex-col sm:mt-12 lg:mt-16 md:w-40  lg:w-80">
-              <h4 className="text-white sm:text-lg text-2xl">Shonen</h4>
-              <p className="text-white sm:w-40 xl:w-80 text-xs">Is the manga that is aimed at adolescent boys. They are series with large amounts of action, in which humorous situations often occur. The camaraderie between members of a collective or a combat team stands out.</p>
+              <h4 className="text-white sm:text-lg text-3xl">{categories[counter]?.name}</h4>
+              <p className="text-white sm:w-40 xl:w-80 text-md">{categories[counter]?.description}</p>
             </div>
-            <div className="w-10 self-center">
+            <div onClick={next} className="w-10 self-center">
               <svg xmlns="http://www.w3.org/2000/svg" fill="#FFFFFF80" viewBox="0 0 24 24" strokeWidth=".5" stroke="currentColor" className="w-10 h-10">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
               </svg>
@@ -24,5 +47,3 @@ export default function Carousel() {
       </>
     )
   }
-  
-  
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,19 +1,12 @@
+import Welcome from "../components/Welcome";
+import Carousel from "../components/Carousel";
+
 export default function Index() {
-    return (
-      <>
-        <div className="flex justify-center xsm:hidden">
-          <div className="bg-cover bg-background_index w-[100vw] h-[65vh] sm:bg-center flex items-center justify-center">
-            <div>
-              <h1 className="text-white md:text-center sm:text-4xl md:text-6xl mb-2">Your favorite comic book store ✨</h1>
-              <h4 className="text-white text-xl  sm:flex md:flex sm:justify-center md:justify-center lg:flex mb-2">Explore our catalog to live the adventure of your life</h4>
-              <div className="md:flex sm:flex sm:justify-center md:justify-center">
-                <button className="rounded-lg bg-gradient-to-r from-[#4338CA] to-[#5E52F3] w-[12vw] h-[5vh] sm:w-[20vw] text-white text-2xl">Let’s go!</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </>
-    )
-  }
-  
-  
+  return ( 
+    <>
+      <Welcome />
+      <Carousel />
+    </>
+  )
+}
+
